2a99bf9a396a441c64b26b8c8ecbf55d5a1c9272
Kent0710 <christiankent001@gmail.com>
2024-04-14 18:48:39 +0800

    Edited middlware file 2

--- middleware.ts
+++ middleware.ts
@@ -4,10 +4,10 @@ export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
 
   // this is for production
-  // const session = !!req.cookies.get("__Secure-next-auth.session-token")
+  const session = !!req.cookies.get("__Secure-next-auth.session-token")
 
   // this is for development
-  const session = !!req.cookies.get("next-auth.session-token")
+  // const session = !!req.cookies.get("next-auth.session-token")
 
   if (!session) {
     return NextResponse.redirect(new URL(`/api/auth/signin?callbackUrl=${path}`, req.url));
@@ -16,8 +16,5 @@ export default async function middleware(req: NextRequest) {
 }
 
 export const config = {
-    matcher : [ 'https://flow-amber.vercel.app/dashboard', 
-    'https://flow-amber.vercel.app/createNew', 
-    '/projects/:path*',
-  ],
+    matcher : [ 'https://flow-amber.vercel.app/dashboard', 'https://flow-amber.vercel.app/createNew', 'https://flow-amber.vercel.app/projects/:path*'],
 }
